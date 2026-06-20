a9d71a5b61b0f2c5eab3d461bb750154d240d260
Aragaan <matheuslm2009tbm@gmail.com>
2026-06-20 10:53:09 -0300

    Remove unused mostrarMensagem function
    Removed the mostrarMensagem function and alert.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function mostrarMensagem(){
+const header = document.getElementById("header");
 
-alert(
-"Obrigado por visitar o site!"
-);
+window.addEventListener("scroll", () => {
 
-}
+    if(window.scrollY > 50){
+        header.classList.add("scrolled");
+    }else{
+        header.classList.remove("scrolled");
+    }
+
+});
+
+const mobileBtn = document.getElementById("mobile-btn");
+const nav = document.getElementById("nav");
+
+mobileBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
